2b7b390f5870c3eac14f14139f09b5e5f13227ae
Qweffy <nicolasmastakas@gmail.com>
2024-01-24 11:15:05 -0300

    feat: fetch and typing

---
+++ src/types/githubAPI.types.ts
@@ -0,0 +1,83 @@
+export type UserSummary = {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+}
+
+export type UserDetails = {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+  name: string
+  company: string | null
+  blog: string
+  location: string
+  email: string | null
+  hireable: boolean | null
+  bio: string | null
+  twitter_username: string | null
+  public_repos: number
+  public_gists: number
+  followers: number
+  following: number
+  created_at: string
+  updated_at: string
+}
+
+export type Repository = {
+  id: number
+  node_id: string
+  name: string
+  full_name: string
+  private: boolean
+  owner: UserSummary
+  html_url: string
+  description: string | null
+  fork: boolean
+  url: string
+}
+
+export type Organization = {
+  login: string
+  id: number
+  node_id: string
+  url: string
+  repos_url: string
+  events_url: string
+  hooks_url: string
+  issues_url: string
+  members_url: string
+  public_members_url: string
+  avatar_url: string
+  description: string
+}
